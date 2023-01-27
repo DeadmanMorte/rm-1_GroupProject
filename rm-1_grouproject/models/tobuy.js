4e1c10bfb997263473566b137752a2e076ff8a3c
Isaac Kim <kisaac95@yahoo.com>
2023-01-26 21:14:53 -0800

    added controller and models

---
+++ rm-1_grouproject/models/tobuy.js
@@ -0,0 +1,24 @@
+// DEPENDENCIES
+const { Sequelize, DataTypes, Model } = require('sequelize');
+const sequelize = new Sequelize(process.env.PG_URI);
+
+// MODEL 
+class ToBuy extends Model {
+    static association(){
+        ToBuy.belongsTo({})
+    }
+};
+
+ToBuy.init({
+    tobuy_item:{
+        type:DataTypes.STRING,
+        allowNull:true
+    }
+}, {
+    sequelize,
+    modelName: 'ToBuy',
+    tableName: 'to_buy_list'
+});
+
+// EXPORT 
+module.exports = ToBuy;
---
+++ rm-1_grouproject/models/tochat.js
@@ -0,0 +1,24 @@
+// DEPENDENCIES
+const { Sequelize, DataTypes, Model } = require('sequelize');
+const sequelize = new Sequelize(process.env.PG_URI);
+
+// MODEL 
+class ToChat extends Model {
+    static association(){
+        ToChat.belongsTo({})
+    }
+};
+
+ToChat.init({
+    tobuy_item:{
+        type:DataTypes.STRING,
+        allowNull:true
+    }
+}, {
+    sequelize,
+    modelName: 'ToChat',
+    tableName: 'to_chat_list'
+});
+
+// EXPORT 
+module.exports = ToChat;
