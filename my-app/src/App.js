491ee407344dd50089d87edb5202cc64756bdcba
AnthonyD9527 <106792017+AnthonyD9527@users.noreply.github.com>
2023-01-26 17:10:09 -0800

    Added 4 components and filled custom-form

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 
+// components
+import CustomForm from './components/CustomForm';
+
 function App() {
   return (
     <div className="App">
---
+++ my-app/src/components/CustomForm.js
@@ -0,0 +1,48 @@
+import { useState } from "react";
+
+const CustomForm = ({ addTask }) => {
+    const [task, setTask] = useState("");
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+        addTask({
+            name: task,
+            checked: false,
+            id: Date.now()
+        })
+        setTask("")
+    }
+
+    return (
+        <form
+        className="todo"
+        onSubmit={handleFormSubmit}
+        >
+            <div className="wrapper">
+                <input
+                type="text"
+                id="task"
+                className="input"
+                value={task}
+                onInput={(e) => setTask(e.target.value)}
+                required
+                autoFocus
+                maxLength={60}
+                placeholder="Enter Task"
+                />
+                <label 
+                htmlFor="task"
+                className="label"
+                >Enter Task</label>
+                </div>
+                <button 
+                className="btn"
+                aria-label="Add Task"
+                type="submit"
+                >
+                    <PlusIcon/>
+                </button>
+        </form>
+    )
+}
+export default CustomForm
