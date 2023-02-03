20050619d6e397b9da067ed6edd4c50d69e16fe3
Samad <samadafzal1@g.ucla.edu>
2023-02-02 19:07:41 -0800

    use effect added - should be able to fetch from back end

--- my-app/src/components/Display.js
+++ my-app/src/components/Display.js
@@ -2,3 +2,18 @@ import React, { useState, useEffect } from 'react';
 import ToDoList from './toDoList';
 import ToDoEdit from './toDoEdit';
 import CustomForm from './CustomForm';
+
+const DisplayComponent = () => {
+    const [todos, setTodos] = useState([]);
+    const [selectedTodo, setSelectedTodo] = useState(null);
+    const [showForm, setShowForm] = useState(false);
+  
+    useEffect(() => {
+        // Fetch todos from your backend using fetch or Axios
+        fetch('your_backend_url/todos')
+          .then(res => res.json())
+          .then(data => {
+            setTodos(data);
+          })
+          .catch(err => console.error(err));
+      }, []);
