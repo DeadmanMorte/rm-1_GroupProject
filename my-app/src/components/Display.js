0503f08dc1eb2721cbd589c91095375c634966f0
Samad <samadafzal1@g.ucla.edu>
2023-02-02 19:09:43 -0800

    AddTodo post fetch created

--- my-app/src/components/Display.js
+++ my-app/src/components/Display.js
@@ -17,3 +17,20 @@ const DisplayComponent = () => {
           })
           .catch(err => console.error(err));
       }, []);
+
+      const handleAddTodo = todo => {
+        // POST request to your backend to add a todo
+        fetch('your_backend_url/todos', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(todo),
+        })
+          .then(res => res.json())
+          .then(data => {
+            setTodos([...todos, data]);
+            setShowForm(false);
+          })
+          .catch(err => console.error(err));
+      };
