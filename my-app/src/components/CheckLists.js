d5260e4a7bb5e1335f76c5d2af5c7d06ae76b750
AnthonyD9527 <106792017+AnthonyD9527@users.noreply.github.com>
2023-02-02 17:43:23 -0800

    added 4 new components and some minor css changes

--- my-app/src/components/CheckLists.js
+++ my-app/src/components/CheckLists.js
@@ -6,6 +6,7 @@ const CheckLists = () => {
       { ListName: 'Finish Back End', Task1: 'lorem ipsum...', Task2: 'lorem ipsum', Task3: 'lorem ipsum', Task4: 'lorem ipsum', id: 2 },
       { ListName: 'Finish Data Base', Task1: 'lorem ipsum...', Task2: 'lorem ipsum', Task3: 'lorem ipsum', Task4: 'lorem ipsum', id: 3 }
     ]);
+    
 
     return(
         <div className="checklists">
